729408215aad1874b05a373b3121c4a98aab608f
ZhiOne <myspacetrack23@gmail.com>
2020-05-27 15:16:46 +0300

    layout

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,4 @@
-import Layout from '../../components/Layout'
+import Layout from '../../components/layout'
 import Head from "next/head";
 import Date from "../../components/date";
 import { getAllPostIds, getPostData } from '../../lib/posts';
